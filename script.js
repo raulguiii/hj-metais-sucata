2ffd41d4a9a34b2ee6a5d7bcc611e2b831b77904
raulguiii <raul.rodrigues@aluno.faculdadeimpacta.com.br>
2025-07-25 21:43:58 -0300

    abas funcionando mas teve que tirar o right pois estava dando pau

--- script.js
+++ script.js
@@ -22,20 +22,3 @@ themeToggler.addEventListener('click', () => {
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 })
-
-// Fill ordes in table
-
-Orders.forEach(order => {
-    const tr = document.createElement('tr');
-    const trContent = `
-        <tr>
-            <td>${order.productNamer}</td>
-            <td>${order.productNumber}</td>
-            <td>${order.paymenStatus}</td>
-            <td class="warning">${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pendig' ? 'warning' : 'primary'}</td>
-            <td class="primary">Details</td>
-        </tr> `
-
-        tr.innerHTML = trContent;
-        document.querySelector('table body').appendChild('tr');
-})
